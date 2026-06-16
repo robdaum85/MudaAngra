674979f13cf75e4c1c2fdad7d1205a580ec89d9a
Robson Junior <robdaum85@gmail.com>
2026-06-16 14:18:09 -0300

    alteracao fotos presidente

--- src/data/participants.ts
+++ src/data/participants.ts
@@ -1,15 +1,15 @@
 import flavioBolsonaroPhoto from "../assets/participants/flavio-bolsonaro.png";
-import jairBolsonaroPhoto from "../assets/participants/jair-bolsonaro.png";
+import jairBolsonaroPhoto from "../assets/jair.jpg";
 
 export const participants = [
-  {
-    name: "Flávio Bolsonaro",
-    image: flavioBolsonaroPhoto,
-    imageClassName: "object-[43%_28%]",
-  },
   {
     name: "Jair Bolsonaro",
     image: jairBolsonaroPhoto,
     imageClassName: "object-[57%_24%]",
+  },
+  {
+    name: "Flávio Bolsonaro",
+    image: flavioBolsonaroPhoto,
+    imageClassName: "object-[43%_28%]",
   },
 ];
